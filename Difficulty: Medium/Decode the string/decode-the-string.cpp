6775d38d94a0e1fb1005724864bfa76e573b92ec
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-03-01 21:55:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    string decodedString(string &s) {
+ string res = "";
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ']') {
+            res.push_back(s[i]);
+        }
+        else {
+            string temp = "";
+            while (!res.empty() && res.back() != '[') {
+                temp.push_back(res.back());
+                res.pop_back();
+            }
+            reverse(temp.begin(), temp.end());
+            res.pop_back();
+            string num = "";
+            while (!res.empty() && res.back() >= '0' && res.back() <= '9') {
+                num.push_back(res.back());
+                res.pop_back();
+            }
+            reverse(num.begin(), num.end());
+            int p = stoi(num);
+            while (p--) {
+                res.append(temp);
+            }
+        }
+    }
+    return res;
+}
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.decodedString(s) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
