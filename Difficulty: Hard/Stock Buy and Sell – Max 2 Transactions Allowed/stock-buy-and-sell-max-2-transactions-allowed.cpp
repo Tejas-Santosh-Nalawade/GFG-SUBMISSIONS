ff8142f1d8114f91b031f34406ca4fc2f21c4c78
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-03-20 23:01:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+    int firstBuy = INT_MIN;  
+    int firstSell = 0;      
+    int secondBuy = INT_MIN; 
+    int secondSell = 0;      
+    for (int i = 0; i < prices.size(); i++) {
+
+        firstBuy = max(firstBuy, -prices[i]);
+        firstSell = max(firstSell, firstBuy + prices[i]);
+        secondBuy = max(secondBuy, firstSell - prices[i]);
+        secondSell = max(secondSell, secondBuy + prices[i]);
+    }
+    return secondSell;
+}           
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxProfit(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
