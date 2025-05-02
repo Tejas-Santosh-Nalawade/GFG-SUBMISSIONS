4131d593e71e0b052d131e933879fc6a003282c7
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-05-02 15:07:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bitonic Point/bitonic-point.cpp
+++ Difficulty: Easy/Bitonic Point/bitonic-point.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function template for C++
 class Solution {
   public:
     int findMaximum(vector<int> &arr) {
-    sort(arr.begin(),arr.end());
-    return arr[arr.size()-1];
+        int ans = 0;
+        int s=0;
+        int e= arr.size()-1;
+        int mid = 0;
+        while(s<e){
+            mid = s+(e-s)/2;
+            if(mid+1 < arr.size() && arr[mid] > arr[mid+1]){
+                e=mid;
+            } else{
+                s = mid+1;
+            }
+        }
+        return arr[e];
     }
 };
+
 
 //{ Driver Code Starts.
 
