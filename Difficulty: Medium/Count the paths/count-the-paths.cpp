4e6912bc66f3cf4d06aada3c46783b5d8098e7ab
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-06-05 11:21:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the paths/count-the-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int dfs (int u, int dest, vector<vector<int>> &adj, vector<int> &memo){
+        if(u == dest) return 1;
+        if(memo[u] != -1) return memo[u];
+        int count = 0;
+        for(int v:adj[u]){
+            count+= dfs(v, dest, adj, memo);
+        }
+        return memo[u] = count;
+    }
+  
+    int countPaths(vector<vector<int>>& edges, int V, int src, int dest) {
+        vector<vector<int>> adj(V);
+        for(auto e:edges){
+            adj[e[0]].push_back(e[1]);
+        }
+        vector<int>memo (V,-1);
+        return dfs ( src, dest, adj, memo);
+        
+    }
+};
