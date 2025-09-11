76226777c59d9abc2611ebad792ffdc75ef4a88c
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-09-11 23:39:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,54 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int minJumps(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> dp(n, INT_MAX);
-    dp[n - 1] = 0;
+int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        if (n <= 1) return 0;          // Already at last index
+        if (arr[0] == 0) return -1;    // Can't move anywhere
 
-    for (int i = n - 2; i >= 0; i--) {
-        for (int j = i + 1; j <= i + arr[i] && j < n; j++) {
-            if (dp[j] != INT_MAX) {
-                dp[i] = min(dp[i], 1 + dp[j]);
+        int maxReach = arr[0];  // Farthest index reachable
+        int steps = arr[0];     // Steps left in current jump
+        int jumps = 1;          // We make the first jump from index 0
+
+        for (int i = 1; i < n; i++) {
+            if (i == n - 1) return jumps;  // Reached last index
+
+            maxReach = max(maxReach, i + arr[i]);
+            steps--;
+
+            if (steps == 0) {  // Need to make another jump
+                jumps++;
+                if (i >= maxReach) return -1;  // Can't move further
+                steps = maxReach - i;
             }
         }
+        return -1; // If end is never reached
     }
-    if (dp[0] == INT_MAX)
-        return -1;
-
-    return dp[0];
-}
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
