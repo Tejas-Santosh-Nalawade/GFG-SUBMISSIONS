6cfc22bf5b900647ed2bfe390a9bf0d6fed2c53d
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-18 22:03:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+vector<string> generateBinary(int n){   
+    vector<string> res;
+    queue<string> q;
+
+
+    q.push("1");
+
+    while (n--) {
+        
+        string s1 = q.front();
+        q.pop();
+        res.push_back(s1);
+
+        string s2 = s1; 
+        
+        if(q.size() < n) {
+           
+            q.push(s1.append("0"));
+            q.push(s2.append("1"));
+        }
+    }
+    return res;
+}
+};
