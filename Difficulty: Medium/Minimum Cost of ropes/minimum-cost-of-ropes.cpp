bcf538bdab484b712c9a95f48e146196e5e1db90
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-26 17:30:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    long long  minCost(vector<int>& arr) {
+        priority_queue<long long, vector<long long>,greater<long long>> pq;
+        int n =arr.size();
+        for(int i=0;i<n;i++){
+            pq.push(arr[i]);
+        }
+        
+        long long cost=0;
+        
+        while(pq.size()>1){
+            long long a=pq.top();
+            pq.pop();
+            
+            long long b=pq.top();
+            pq.pop();
+            
+            long long sum=a+b;
+            cost+=sum;
+            
+            pq.push(sum);
+            
+        }
+        
+        return cost;
+    }
+};
