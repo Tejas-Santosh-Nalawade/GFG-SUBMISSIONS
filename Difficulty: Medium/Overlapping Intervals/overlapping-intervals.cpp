ba0594867445fe2498739ca5bf3e531e5ab85234
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-12-17 11:32:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -1,57 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    vector<vector<int>> mergeOverlap(vector<vector<int>>& inter) {
-        vector<vector<int>> ans;
-         sort(inter.begin(),inter.end());
-         ans.push_back(inter[0]);
-         int j=0;
-         
-         int n = inter.size();
-         for(int i=1;i<n;i++)
-         {
-             if(ans[j][1]>=inter[i][0])
-             {
-                 ans[j][1] = max(inter[i][1],ans[j][1]);
-             }
-             else if(ans[j][1]<inter[i][0])
-             {
-                 ans.push_back(inter[i]);
-                 j++;
-             }
-         }
-         return ans;
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>res;
+        res.push_back(arr[0]);
+        
+        for(int i=1; i<n; i++){
+            vector<int>&last = res.back();
+            vector<int>&current = arr[i];
+            
+            if(current[0] <= last[1]){
+                last[1] = max(last[1], current[1]);
+            }
+            else{
+                 res.push_back(current);
+            }  
+        }
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> arr(n);
-        for (int i = 0; i < n; i++) {
-            int x, y;
-            cin >> x >> y;
-            arr[i].push_back(x);
-            arr[i].push_back(y);
-        }
-        Solution obj;
-        vector<vector<int>> ans = obj.mergeOverlap(arr);
-        for (auto i : ans) {
-            for (auto j : i) {
-                cout << j << " ";
-            }
-        }
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
