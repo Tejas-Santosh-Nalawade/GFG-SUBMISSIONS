4e9ad175e9db6a9a69cadf86353d115701ed3e9f
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-01-12 17:42:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,67 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
-        vector<int> res;
-        int n=arr.size();
-        multiset<int> s;
-        int wS=0;
-        int wE=0;
-        while(wE<k){
-            s.insert(arr[wE]);
-            wE++;
+vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+    vector<int> res;
+ 
+    deque<int> dq(k);
+
+    for (int i = 0; i < k; ++i) {
+      
+        while (!dq.empty() && arr[i] >= arr[dq.back()]) {
+          
+            dq.pop_back();
         }
-        res.push_back(*s.rbegin());
-        while(wE<n){
-            s.erase(s.find(arr[wS]));
-            s.insert(arr[wE]);
-            res.push_back(*s.rbegin());
-            wS++;
-            wE++;
-        }
-        return res;
-    }
-};
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution obj;
-        vector<int> res = obj.maxOfSubarrays(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
+        dq.push_back(i);
     }
 
-    return 0;
+    for (int i = k; i < arr.size(); ++i) {
+     
+        res.push_back(arr[dq.front()]);
+
+        while (!dq.empty() && dq.front() <= i - k) {
+          
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && arr[i] >= arr[dq.back()]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+    }
+    res.push_back(arr[dq.front()]);
+
+    return res;
 }
-
-// } Driver Code Ends
+};
