f94aeee0782998f68466583e6171cfb186982b2e
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-01-21 19:14:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,60 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<int> calculateSpan(vector<int>& arr) {
-        vector<int> v;
-        int n=arr.size();
-       stack<pair<int,int>>st;
-       for(int i=0;i<n;i++){
-           
-           if(st.size()==0) v.push_back(-1);
-           else if(st.size()>0 && st.top().first > arr[i])
-                   v.push_back(st.top().second);
-           else if(st.size()>0 && st.top().first <= arr[i])
-           {
-               while(st.size()>0 && st.top().first <= arr[i])
-                      st.pop();
-            if(st.size() == 0) v.push_back(-1);
-            else v.push_back(st.top().second);
-           } 
-           st.push({arr[i],i});
-       }
-       for(int i=0;i<v.size();i++)
-           v[i] = i - v[i];
-       return v;
+    int n = arr.size(); 
+    vector<int> span(n);  
+    stack<int> st;  
+    for (int i = 0; i < n; i++) {
+        
+        while (!st.empty() && arr[st.top()] <= arr[i]) {
+            st.pop();
+        }
+
+        if (st.empty()) {
+            span[i] = (i + 1);
+        }
+        else {
+            span[i] = (i - st.top());
+        }
+
+        st.push(i);
     }
 
-
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> ans = obj.calculateSpan(arr);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
+    return span;
 }
-// } Driver Code Ends
+};
