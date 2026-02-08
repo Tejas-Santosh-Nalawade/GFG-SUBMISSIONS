1a777eaa4c759fd1e1077e5428bd59fe5156cbf4
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-02-08 23:53:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -1,68 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
-// User function Template for C++
 class Solution {
   public:
-    // Function to find maximum product subarray
-    int maxProduct(vector<int> &arr) {
-       int maxx= arr[0],minn = arr[0], prd = arr[0];
-        for (int i = 1; i < arr.size(); i++) {
+int maxProduct(vector<int> &arr) {
+        int n=arr.size();
+        int pre=1, suf=1, maxi=-1e9;
         
-             if(arr[i] < 0){
-                swap(minn, maxx);
-            }else if(arr[i] == 0){
-                minn = maxx = 1;
-            }
-            
-            minn = minn * arr[i];
-            minn = min(minn, arr[i]);
-            
-            maxx = maxx * arr[i];
-            maxx = max(arr[i], maxx);
-            
-            prd = max(prd, maxx);
+        for(int i=0; i<n; i++){
+            pre*=arr[i];
+            suf*=arr[n-i-1];
+            maxi=max(maxi, max(pre, suf));
+            if(arr[i]==0) pre=1;
+            if(arr[n-i-1]==0) suf=1;
         }
         
-        return prd;
+        return maxi;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        // int n, i;
-        // cin >> n;
-        // vector<int> arr(n);
-        // for (i = 0; i < n; i++) {
-        //     cin >> arr[i];
-        // }
-
-        vector<int> arr;
-        string input;
-
-        // Read array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution ob;
-        auto ans = ob.maxProduct(arr);
-        cout << ans << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
