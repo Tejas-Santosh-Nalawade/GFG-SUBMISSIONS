0c0fc164072985885f05b81f6396cf4ca35ee9a2
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-02-11 18:23:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Equalize the Towers/equalize-the-towers.cpp
+++ Difficulty: Medium/Equalize the Towers/equalize-the-towers.cpp
@@ -1,53 +1,45 @@
 class Solution {
   public:
-  int findCost(vector<int> &heights, vector<int> &cost, int h)
-    {
-        int res = 0;
-        int n = heights.size();
-        for (int i = 0; i < n; i++)
-        {
-            res += cost[i] * abs(heights[i] - h);
-        }   
-        return res;
+ int findCost(vector<int> &heights, vector<int> &cost, int h) {
+    int res = 0;
+    int n = heights.size();
+    for (int i = 0; i < n; i++) {
+        res += cost[i] * abs(heights[i] - h);
     }
-    int minCost(vector<int>& heights, vector<int>& cost) {
-        int n = heights.size();
-        int mini = INT_MAX, maxi = INT_MIN;
-        for (auto val : heights)
-        {
-            mini = min(mini, val);
-            maxi = max(maxi, val);
-        }   
+    return res;
+}
 
-        int low = mini, high = maxi;
-        int ans;
+int minCost(vector<int> &heights, vector<int> &cost) {
+    int n = heights.size();
 
-        while (low <= high)
-        {
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (auto val : heights) {
+        mini = min(mini, val);
+        maxi = max(maxi, val);
+    }
 
-            int mid = low + (high - low) / 2;
+    int low = mini, high = maxi;
+    int ans;
 
-            int val1 = findCost(heights, cost, mid - 1);
-            int val2 = findCost(heights, cost, mid);
-            int val3 = findCost(heights, cost, mid + 1);
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-            if (val2 <= val1 && val2 <= val3)
-            {
-                ans = val2;
-                break;
-            }
+        int val1 = findCost(heights, cost, mid - 1);
+        int val2 = findCost(heights, cost, mid);
+        int val3 = findCost(heights, cost, mid + 1);
 
-            else if (val1 >= val2 && val2 >= val3)
-            {
-                low = mid + 1;
-            }
-
-            else if (val2 >= val1 && val3 >= val2)
-            {
-                high = mid - 1;
-            }
+        if (val2 <= val1 && val2 <= val3) {
+            ans = val2;
+            break;
         }
-
-        return ans;
+        else if (val1 >= val2 && val2 >= val3) {
+            low = mid + 1;
+        }
+        else if (val2 >= val1 && val3 >= val2) {
+            high = mid - 1;
+        }
     }
+
+    return ans;
+}
 };
