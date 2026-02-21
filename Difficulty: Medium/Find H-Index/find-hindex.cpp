0d32093d9d74a8d99ece40f30249ab18e6302836
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-02-21 23:14:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,24 +1,15 @@
-class Solution {
+ class Solution {
   public:
     int hIndex(vector<int>& citations) {
-    int n = citations.size();
-    vector<int> freq(n + 1);
-
-    for (int i = 0; i < n; i++) {
-        if (citations[i] >= n)
-            freq[n] += 1;
-        else
-            freq[citations[i]] += 1;
+        // code here
+         int n = citations.size();
+        sort(citations.begin(), citations.end());
+        int ans=0;
+        
+         for(int i=0; i<n; i++){
+            ans = max(ans, min(citations[i], n-i));
+        }
+    
+        return ans;
     }
-
-    int idx = n;
-
-    int s = freq[n]; 
-    while (s < idx) {
-        idx--;
-        s += freq[idx];
-    }
-  
-    return idx;
-}
 };
