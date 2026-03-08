95dacb5243b82d7c2c3019b61ad12f5299b9b426
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-03-08 20:58:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.cpp
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.cpp
@@ -1,17 +1,23 @@
 class Solution {
   public:
     bool pythagoreanTriplet(vector<int>& arr) {
-        int n = arr.size();
+  	int n = arr.size();
     int maxEle = 0;
     for (int ele: arr) 
         maxEle = max(maxEle, ele);
+
     vector<bool> vis(maxEle + 1, 0);
+
     for (int ele: arr)
         vis[ele] = true;
+
     for (int a = 1; a < maxEle + 1; a++) {
+
         if (vis[a] == false)
             continue;
+
         for (int b = 1; b < maxEle + 1; b++) {
+
         	if (vis[b] == false)
             	continue;
 
@@ -19,6 +25,7 @@ class Solution {
 
             if ((c * c) != (a * a + b * b) || c > maxEle)
                 continue;
+
             if (vis[c] == true) {
                 return true;
             }
