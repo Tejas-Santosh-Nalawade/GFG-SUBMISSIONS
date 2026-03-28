1e8e7a3f8fdc326357e519a3c448eca2b76521ce
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-03-28 23:39:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
+++ Difficulty: Hard/Articulation Point - II/articulation-point-ii.cpp
@@ -1,119 +1,61 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
-vector<vector<int>> constructAdj(int V, vector<vector<int>> &edges) {
-    vector<vector<int>> adj(V); 
+private:
+    int timer = 0;
     
-    for (auto &edge : edges) {
-       
-        adj[edge[0]].push_back(edge[1]);
-        adj[edge[1]].push_back(edge[0]);
-    }
-    return adj;
-}
-
-// Helper function to perform DFS and find articulation points
-// using Tarjan's algorithm.
-void findPoints(vector<vector<int>> &adj, int u, vector<int> &visited,
-                vector<int> &disc, vector<int> &low, 
-                int &time, int parent, vector<int> &isAP) {
-                    
-    // Mark vertex u as visited and assign discovery
-    // time and low value
-    visited[u] = 1;
-    disc[u] = low[u] = ++time;
-    int children = 0; 
-
-    // Process all adjacent vertices of u
-    for (int v : adj[u]) {
+    void dfs(int u, int parent, vector<int> adj[], vector<int>& disc, 
+             vector<int>& low, vector<bool>& isAP) {
+        disc[u] = low[u] = timer++;
+        int children = 0;
         
-        // If v is not visited, then recursively visit it
-        if (!visited[v]) {
-            children++;
-            findPoints(adj, v, visited, disc, low, time, u, isAP);
-
-            // Check if the subtree rooted at v has a 
-            // connection to one of the ancestors of u
-            low[u] = min(low[u], low[v]);
-
-            // If u is not a root and low[v] is greater than or equal to disc[u],
-            // then u is an articulation point
-            if (parent != -1 && low[v] >= disc[u]) {
-                isAP[u] = 1;
+        for (int v : adj[u]) {
+            if (disc[v] == -1) {
+                children++;
+                dfs(v, u, adj, disc, low, isAP);
+                
+                low[u] = min(low[u], low[v]);
+                
+                if (parent == -1 && children > 1) {
+                    isAP[u] = true;
+                }
+                if (parent != -1 && low[v] >= disc[u]) {
+                    isAP[u] = true;
+                }
+            } else if (v != parent) {
+                low[u] = min(low[u], disc[v]);
             }
-        } 
-        
-        // Update low value of u for back edge
-        else if (v != parent) {
-            low[u] = min(low[u], disc[v]);
         }
     }
-
-    // If u is root of DFS tree and has more than 
-    // one child, it is an articulation point
-    if (parent == -1 && children > 1) {
-        isAP[u] = 1;
-    }
-}
-
-// Main function to find articulation points in the graph
-vector<int> articulationPoints(int V, vector<vector<int>> &edges) {
     
-    vector<vector<int>> adj = constructAdj(V, edges);
-    vector<int> disc(V, 0), low(V, 0), visited(V, 0), isAP(V, 0);
-    int time = 0; 
-
-    // Run DFS from each vertex if not
-    // already visited (to handle disconnected graphs)
-    for (int u = 0; u < V; u++) {
-        if (!visited[u]) {
-            findPoints(adj, u, visited, disc, low, time, -1, isAP);
+public:
+    vector<int> articulationPoints(int V, vector<vector<int>>& edges) {
+        vector<int> adj[V];
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
-    }
-
-    // Collect all vertices that are articulation points
-    vector<int> result;
-    for (int u = 0; u < V; u++) {
-        if (isAP[u]) {
-            result.push_back(u);
+        
+        vector<int> disc(V, -1);
+        vector<int> low(V);
+        vector<bool> isAP(V, false);
+        timer = 0;
+        
+        for (int i = 0; i < V; i++) {
+            if (disc[i] == -1) {
+                dfs(i, -1, adj, disc, low, isAP);
+            }
         }
+        
+        vector<int> result;
+        for (int i = 0; i < V; i++) {
+            if (isAP[i]) {
+                result.push_back(i);
+            }
+        }
+        
+        if (result.empty()) {
+            result.push_back(-1);
+        }
+        
+        return result;
     }
-
-    // If no articulation points are found, return vector containing -1
-    return result.empty() ? vector<int>{-1} : result;
-}
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> edges;
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-        Solution obj;
-        vector<int> ans = obj.articulationPoints(V, edges);
-        sort(ans.begin(), ans.end());
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
