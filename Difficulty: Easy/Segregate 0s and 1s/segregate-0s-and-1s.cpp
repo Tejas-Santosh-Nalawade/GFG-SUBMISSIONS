22692c7ae8b7672a058db34be4c0957cddaeef17
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-04-08 22:55:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        int countZero = 0;
+        for (auto i : arr) {
+            if (!i) {
+               countZero++; 
+            }
+        }
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (countZero) {
+                arr[i] = 0;
+                countZero--;
+            } else {
+                arr[i] = 1;
+            }
+        }
+    }
+};
