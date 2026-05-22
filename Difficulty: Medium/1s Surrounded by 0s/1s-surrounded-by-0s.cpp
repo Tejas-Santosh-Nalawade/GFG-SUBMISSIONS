370ca33eedf1b3df84e108a2f5fb7e82b1a2bb14
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-05-22 15:36:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/1s Surrounded by 0s/1s-surrounded-by-0s.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+void dfs(int i, int j, vector<vector<int>> &grid)
+{
+    if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0)
+    {
+        return;
+    }
+
+    grid[i][j] = 0;
+
+    int rowDir[4] = {1, 0, -1, 0};
+    int colDir[4] = {0, 1, 0, -1};
+
+    for (int k = 0; k < 4; k++)
+    {
+        dfs(i + rowDir[k], j + colDir[k], grid);
+    }
+}
+
+int cntOnes(vector<vector<int>> &grid)
+{
+    int n = grid.size();
+    if (n == 0)
+        return 0;
+    int m = grid[0].size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (grid[i][0] == 1)
+            dfs(i, 0, grid);
+        if (grid[i][m - 1] == 1)
+            dfs(i, m - 1, grid);
+    }
+
+    for (int j = 0; j < m; j++)
+    {
+        if (grid[0][j] == 1)
+            dfs(0, j, grid);
+        if (grid[n - 1][j] == 1)
+            dfs(n - 1, j, grid);
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            count += grid[i][j];
+        }
+    }
+
+    return count;
+}
+};
