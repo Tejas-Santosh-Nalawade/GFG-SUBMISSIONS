9b3495dcdd90c859006c70cfd70a176a4808f45b
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-05-24 08:33:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last Coin in a Game of Alternates/last-coin-in-a-game-of-alternates.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int coin(vector<int>& arr) {
+        int i=0, j = arr.size() - 1;
+        while(i<j){
+            if(arr[i] > arr[j]) i++;
+            else j--;
+        }
+        return arr[i];
+        
+    }
+};
