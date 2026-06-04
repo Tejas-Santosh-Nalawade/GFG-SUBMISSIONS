8238f5f6266d47f9388c60e08c74a33694f01e3a
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-06-04 21:34:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substring with Max Zero-One Diff/substring-with-max-zero-one-diff.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxSubstring(string &s) {
+        int n = s.size();
+        int zero = 0;
+        int ans = -1;
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i]=='1')zero--;
+            else zero++;
+            
+            if(zero<=0)zero=0;
+            else ans = max(ans,zero);
+            
+        }
+        
+        return ans;
+    }
+};
