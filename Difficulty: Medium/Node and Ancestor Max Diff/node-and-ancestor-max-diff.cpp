79eaa803ccc642a7f5727c68423edc1d54e5b7fd
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-08-20 00:04:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Node and Ancestor Max Diff/node-and-ancestor-max-diff.cpp
@@ -0,0 +1,34 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+class Solution {
+  public:
+      int solve(Node* root,int &maxi){
+          if(root == NULL){
+              return INT_MAX;
+          }
+          if(!root->left && !root->right){
+              return root->data;
+          }
+          int left = solve(root->left,maxi);
+          int right = solve(root->right,maxi);
+          int b = min(left,right);
+          maxi = max(maxi,root->data-b);
+          int mini = min(root->data,b);
+          return mini;
+      } 
+      int maxDiff(Node* root) {
+          int maxi = INT_MIN;
+          solve(root,maxi);
+          return maxi;
+      }
+  };
+
