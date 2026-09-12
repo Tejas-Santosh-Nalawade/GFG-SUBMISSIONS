7c337bb5155617f4856c4a883adbf9d8ef951210
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-09-12 23:54:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Product Subsequence of Size K/max-product-subsequence-of-size-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr, int k) {
+int n = arr.size();
+		sort(arr.begin(), arr.end());
+		int result = INT_MIN;
+		for (int i = 0; i <= k; ++i) {
+			int product = 1;
+			for (int j = 0; j < k - i; ++j)product *= arr[j];
+			for (int j = n - i; j<n; ++j)product *= arr[j];
+			result = max(result, product);
+		}
+		return result;
+	}
+};
