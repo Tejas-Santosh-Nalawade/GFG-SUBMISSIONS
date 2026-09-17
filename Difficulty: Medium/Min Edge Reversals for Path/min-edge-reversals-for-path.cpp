ce3f43e07cb67c198db5f922885690f60367685e
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-09-17 22:18:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Edge Reversals for Path/min-edge-reversals-for-path.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    int minimumEdgeReversal(vector<vector<int>> &edges, int n, int src, int dst) {
+        unordered_map<int, vector<pair<int, int>>> adj;
+
+        for (auto& edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back({v, 0});
+            adj[v].push_back({u, 1});
+        }
+
+        vector<int> dist(n+1, INT_MAX);
+        queue<pair<int, int>> q;
+        q.push({0, src});
+        dist[src] = 0;
+
+        while (!q.empty()) {
+
+            auto [w, u] = q.front();
+            q.pop();
+
+
+            for (auto& [v, d] : adj[u]) {
+
+                if (w + d < dist[v]) {
+                    dist[v] = w + d;
+                    q.push({w+d, v});
+                }
+            }
+        }
+
+        return (dist[dst] != INT_MAX) ? dist[dst] : -1;
+    }
+};
